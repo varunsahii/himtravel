741768299fc9a25f1e769b75e3c2a74993cc1537
varunsahii <varunsahi1997@gmail.com>
2021-01-30 07:55:16 +0530

    add procfile corrections

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname +"/index.html");
 });
 
-app.listen(3000, function(){
-	console.log("server started on port: 3000");
+app.listen(process.env.PORT || 3000, function(){
+ console.log("server started on port: 3000");
 });
